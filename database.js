3185836d8251d9485fa40bcd644e44cb3ebc9bd5
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2025-12-17 13:20:30 +0530

    database explore

--- database.js
+++ database.js
@@ -1,1 +1,38 @@
-const URI = "mongodb+srv://tusharNode:sLek161ZZl3YC5MS@tusharnode.ym8lebh.mongodb.net/"
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb+srv://tusharNode:sLek161ZZl3YC5MS@tusharnode.ym8lebh.mongodb.net/";
+
+const client = new MongoClient(url);
+
+const dbName = "FirstDB";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+  const data = {
+    firstName: "Mohit",
+    lastName: "Tailor",
+    city: "Silvassa",
+    phnNo: "0123456789",
+  };
+
+  const res = await collection.findOneAndUpdate({firstName: "Mohit"}, { $set: {firstName: "Jack"}});
+  console.log(res)
+//   const insertResult = await collection.insertMany([data]);
+//   console.log("Inserted documents =>", insertResult);
+
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
